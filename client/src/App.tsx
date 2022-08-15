2af6a8aa5f551fe673357f0899e97e9956cecbe9
alann03 <alanhuismann03@gmail.com>
2022-08-15 02:17:27 -0300

    probando commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,20 +2,19 @@ import logo from "./logo.svg";
 import styled from "styled-components";
 
 const AppStyle = styled.div`
-  box-sizing: border-box;
-  width: 100%;
-  height: 100%;
+	box-sizing: border-box;
+	width: 100%;
+	height: 100%;
 `;
-
+//probando commit
 function App() {
-  return (
-    <AppStyle>
-      <header>
-        <p>Listos para PF</p>
-      </header>
-    </AppStyle>
-  );
+	return (
+		<AppStyle>
+			<header>
+				<p>Listos para PF</p>
+			</header>
+		</AppStyle>
+	);
 }
 
 export default App;
-
