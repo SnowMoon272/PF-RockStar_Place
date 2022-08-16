2c03798300cca9f2a4ac78fa1de93c18df1f9224
Manuel Roberto Serrano Torres <CastielAltair0027@outlook.com>
2022-08-16 14:57:49 -0500

    Paleta de Colores Agregada

---
+++ client/src/utils/colors.ts
@@ -0,0 +1,11 @@
+/* Full page colors */
+
+const colors = {
+  Erie_Black: "#18191A",
+  Oxford_Blue: "#14213D",
+  Dark_Cornflower_blue: "#264380",
+  Orange_Web: "#FCA311",
+  Platinum: "#E5E5E5",
+};
+
+export default colors;
