c75d024ea941fbb447d5117c5ad0feb0d0d306cf
Sebastian-pz <spezuluaga@gmail.com>
2022-08-15 21:35:46 -0500

    Se añadió el schema de musicBand

---
+++ api/src/db/schemas/musicBand.ts
@@ -0,0 +1,42 @@
+const { Schema } = require("mongoose");
+
+const musicBandSchema = new Schema({
+    personInCharge : {
+        type: String,
+        trim : true,
+        default : undefined
+    },
+	name: {
+        type : String,
+        trim : true
+    },
+	email: {
+        type:String,
+        unique: true,
+
+    },
+	password: String,
+	rating: Number,
+	reviews: [
+		{
+			comment: { type: String, trim : true },
+			rating: { type: Number, min : 0, max : 5 },
+		},
+	],
+	dates: [
+		{
+			place: String,
+			date: { type: Date, default: Date.now },
+		},
+	],
+	banned: {type:Boolean, default: false},
+    role : {
+        type : String,
+        default : "musicband"
+    }
+});
+
+module.exports = musicBandSchema;
+
+// Social media
+// Leader
