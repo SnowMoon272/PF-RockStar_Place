7afbb36e7eef42d9c37bede1db756fac3dce73e1
Carlos Laprida <carloslapridat@gmail.com>
2022-08-17 18:18:37 -0300

    Rutas agregadas del back

--- api/src/db/models/musicBandModel.ts
+++ api/src/db/models/musicBandModel.ts
@@ -6,127 +6,126 @@ const musicBandSchema = require("../schemas/musicBand");
 const musicBand = model("musicband", musicBandSchema);
 
 interface reviews {
-	author: string;
-	comment: string;
-	rating: number;
+  author: string;
+  comment: string;
+  rating: number;
 }
 interface dates {
-	author: string;
-	place: string;
-	date: Date;
+  author: string;
+  place: string;
+  date: Date;
 }
 enum Roles {
-	ADMIN = "admin",
-	MUSICBAND = "musicband",
-	PLACE = "place",
+  ADMIN = "admin",
+  MUSICBAND = "musicband",
+  PLACE = "place",
 }
 
 type musicBandInterface = {
-	personInCharge: string;
-	name: string;
-	email: string;
-	password: string;
-	rating: number;
-	reviews: reviews[];
-	dates: dates[];
-	banned: boolean;
-	role: Roles;
-	socialMedia: any;
-	description: string;
-	pendingDates: dates[];
-	profilePicture: string;
+  personInCharge: string;
+  name: string;
+  email: string;
+  password: string;
+  rating: number;
+  reviews: reviews[];
+  dates: dates[];
+  banned: boolean;
+  role: Roles;
+  socialMedia: any;
+  description: string;
+  pendingDates: dates[];
+  profilePicture: string;
 };
 
 export const reloadMusicBandRating = async (email: string) => {
-	const userToUpdate = await getMusicBand(email);
-	let sum = 0;
-	for (let review of userToUpdate.reviews) {
-		sum += review.rating;
-	}
-	console.log("Tengo miedo");
-	sum = Math.round((sum / userToUpdate.reviews.length) * 100) / 100;
-	console.log(`Tengo más miedo todavía`);
+  const userToUpdate = await getMusicBand(email);
+  let sum = 0;
+  for (let review of userToUpdate.reviews) {
+    sum += review.rating;
+  }
 
-	try {
-		await musicBand.updateOne({ email }, { rating: sum });
-		console.log("Rating updated");
-	} catch (error) {
-		throw new Error("Error updating rating");
-	}
+  sum = Math.round((sum / userToUpdate.reviews.length) * 100) / 100;
+
+  try {
+    await musicBand.updateOne({ email }, { rating: sum });
+    return { response: "updated" };
+  } catch (error) {
+    throw new Error("Error updating rating");
+  }
 };
 
-export const addReview = async (email: string, review: reviews) => {
-	const userToAddReview = await getMusicBand(email);
+export const addBandReview = async (email: string, review: reviews) => {
+  const userToAddReview = await getMusicBand(email);
 
-	if (userToAddReview) {
-		let previousReviews = userToAddReview.reviews;
-		previousReviews.push(review);
-		try {
-			await musicBand.updateOne({ email }, { reviews: previousReviews });
-			await reloadMusicBandRating(email);
-			return { reviews: previousReviews };
-		} catch (error) {
-			throw new Error("Error creating a review");
-		}
-	} else {
-		throw new Error("User not found");
-	}
+  if (userToAddReview) {
+    let previousReviews = userToAddReview.reviews;
+    previousReviews.push(review);
+    try {
+      await musicBand.updateOne({ email }, { reviews: previousReviews });
+      await reloadMusicBandRating(email);
+      return { reviews: previousReviews };
+    } catch (error) {
+      throw new Error("Error creating a review");
+    }
+  } else {
+    throw new Error("User not found");
+  }
 };
 
-const getMusicBand = async (email: string) => {
-	try {
-		let musicBandResponse = await musicBand.findOne({ email });
-		if (musicBandResponse !== undefined) return musicBandResponse;
-		else return { error: "User not found" };
-	} catch (err: any) {
-		throw new Error("An error occurred getting user");
-	}
+export const getMusicBand = async (email: string) => {
+  try {
+    let musicBandResponse = await musicBand.findOne({ email });
+    if (musicBandResponse !== undefined) return musicBandResponse;
+    else return { error: "User not found" };
+  } catch (err: any) {
+    throw new Error("An error occurred getting user");
+  }
 };
 
 const encodePassword = async (password: string) => {
-	const salt = await bcrypt.genSalt(6);
-	const encodedPassword = await bcrypt.hash(password, salt);
-	return encodedPassword;
+  const salt = await bcrypt.genSalt(6);
+  const encodedPassword = await bcrypt.hash(password, salt);
+  return encodedPassword;
 };
 
 const comparePassword = async (password: string, encodedPassword: string) => {
-	let valid = await bcrypt.compare(password, encodePassword);
-	return valid;
+  let valid = await bcrypt.compare(password, encodePassword);
+  return valid;
 };
 
 export const createMusicBand = async (newMusicBand: musicBandInterface) => {
-	newMusicBand.password = await encodePassword(newMusicBand.password);
-	newMusicBand.rating = 5;
+  newMusicBand.password = await encodePassword(newMusicBand.password);
+  newMusicBand.rating = 5;
 
-	console.log("I arrived is in created music band function");
+  console.log("I arrived is in created music band function");
 
-	try {
-		await musicBand.create(newMusicBand);
-		console.log("New user band created");
-	} catch (error: any) {
-		console.log(error);
-	}
+  try {
+    await musicBand.create(newMusicBand);
+    console.log("New user band created");
+  } catch (error: any) {
+    console.log(error);
+  }
 };
 
 export const getAllMusicBands = async () => {
-	try {
-		const allMusicBands = await musicBand.find(
-			{},
-			{ email: 1, name: 1, rating: 1, description: 1 }
-		);
-		return allMusicBands;
-	} catch (error) {
-		throw new Error("Error getting all music bands ");
-	}
+  try {
+    const allMusicBands = await musicBand.find(
+      {},
+      { email: 1, name: 1, rating: 1, description: 1 },
+    );
+    return allMusicBands;
+  } catch (error) {
+    throw new Error("Error getting all music bands ");
+  }
 };
 
 export const banHandler = async (email: string) => {
-	try {
-		const userToChange = await musicBand.findOne({ email });
-		userToChange.banned === false
-			? await musicBand.updateOne({ email }, { banned: true })
-			: await musicBand.updateOne({ email }, { banned: false });
-	} catch (error) {
-		console.log("Something went wrong in ban function");
-	}
+  try {
+    const userToChange = await musicBand.findOne({ email });
+    userToChange.banned === false
+      ? await musicBand.updateOne({ email }, { banned: true })
+      : await musicBand.updateOne({ email }, { banned: false });
+  } catch (error) {
+    console.log("Something went wrong in ban function");
+  }
 };
--- api/src/db/schemas/placeSchema.ts
+++ api/src/db/schemas/placeSchema.ts
@@ -1,41 +1,49 @@
-const placeSchema = new Schema({
-	capacity: { type: String, trim: true, require: true },
+const { Schema } = require("mongoose");
 
-	name: { type: String, trim: true, require: true },
+export const placeSchema = new Schema({
+  capacity: { type: String, trim: true, require: true },
 
-	city: { type: String, trim: true, require: true },
+  name: { type: String, trim: true, require: true },
 
-	adress: { type: String, trim: true, require: true },
+  city: { type: String, trim: true, require: true },
 
-	rating: { type: Number, default: 0 },
+  adress: { type: String, trim: true, require: true },
 
-	description: { type: String, trim: true, default: "" },
+  rating: { type: Number, default: 0 },
 
-	reviews: [
-		{
-			author: { type: String, trim: true, require: false },
-			review: { type: String, trim: true, require: false },
-			rating: Number,
-		},
-	],
-	dates: [
-		{
-			date: { type: Date },
-			musicBand: { type: String },
-		},
-	],
-	socialMedia: {
-		instagram: { type: String, trim: true, default: undefined },
-		spotify: { type: String, trim: true, default: undefined },
-		youtube: { type: String, trim: true, default: undefined },
-	},
-	pendingDates: [
-		{
-			place: String,
-			date: { type: Date, default: Date.now },
-		},
-	],
-	profilePicture: { type: String, require: false },
+  description: { type: String, trim: true, default: "" },
+
+  banned: { type: Boolean, default: false },
+  role: {
+    type: String,
+    default: "place",
+  },
+
+  reviews: [
+    {
+      author: { type: String, trim: true, require: false },
+      review: { type: String, trim: true, require: false },
+      rating: Number,
+    },
+  ],
+  dates: [
+    {
+      date: { type: Date },
+      musicBand: { type: String },
+    },
+  ],
+  socialMedia: {
+    instagram: { type: String, trim: true, default: undefined },
+    spotify: { type: String, trim: true, default: undefined },
+    youtube: { type: String, trim: true, default: undefined },
+  },
+  pendingDates: [
+    {
+      place: String,
+      date: { type: Date, default: Date.now },
+    },
+  ],
+  profilePicture: { type: String, require: false },
 });
 
 module.exports = placeSchema;
