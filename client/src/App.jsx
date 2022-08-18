dd107fc8c50a3e6ff8cdc074a2cc358454c9c413
Federico Silva Flores <federicosilvaflores@gmail.com>
2022-08-18 15:53:36 -0300

    Componentes Help y Faqs y Rutas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import styled from "styled-components";
 import HomeUNL from "./Components/Home/HomeUNL";
 import "./App.css";
+import Help from "./Components/Help/Help";
+import Faqs from "./Components/Faqs/Faqs";
 
 const AppStyle = styled.div`
   width: 100%;
@@ -14,6 +16,8 @@ function App() {
     <AppStyle>
       <Routes>
         <Route exact path="/" element={<HomeUNL />} />
+        <Route exact path="/help" element={<Help />} />
+        <Route exact path="/faqs" element={<Faqs />} />
       </Routes>
     </AppStyle>
   );
