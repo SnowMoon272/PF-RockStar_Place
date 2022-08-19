bb8c7ecfbc56448bbc268a6ee2e41b9713a2dffd
Sebastian-pz <spezuluaga@gmail.com>
2022-08-18 23:56:30 -0500

    Add testing

---
+++ api/src/tests/musicbandTests/addreview.musicband.test.ts
@@ -0,0 +1,58 @@
+const { createMusicBand, deleteMusicBand, addBandReview, getMusicBand } = require("../../db/models/musicBandModel");
+const mongoose = require("mongoose")
+const connect = require("../../db/db");
+
+export let musicBandTest = {
+  personInCharge: "Sebastian",
+  name: "Test de banda o músico",
+  email: "soyunTest1@gmail.com",
+  password: "test",
+  rating: 5,
+  reviews: [],
+  dates: [],
+  banned: false,
+  role: "musicband",
+  description: "Esta es una descripcion de un test",
+  pendingDates: [],
+  profilePicture: "",
+};
+
+let testReviews = {
+  author: "esteEsUnCorreo@gmail.com",
+  comment: "Increible",
+  rating: 5
+}
+
+let band : any;
+
+beforeAll( async () => {
+  await connect();
+  await deleteMusicBand(musicBandTest.email);
+  band = await createMusicBand(musicBandTest);
+});
+
+afterAll(async () => {
+  await deleteMusicBand(musicBandTest.email);
+  await mongoose.connection.close();
+})
+
+test("band should to be object", () => {
+  expect(typeof band).toBe("object");
+});
+test("band should to have property email", () => {
+  expect(band).toHaveProperty("email");
+});
+test("band should to have property reviews", () => {
+  expect(band).toHaveProperty("reviews");
+});
+test("band.reviews length should be 0", () => {
+  expect(band.reviews).toHaveLength(0);
+});
+test("return of addBandReview should to have property reviews", async () => {
+  const test = await addBandReview(musicBandTest.email, testReviews)
+  expect(test).toHaveProperty("reviews");
+});
+test("band.reviews length should be 1", async () => {
+  let test = await getMusicBand(musicBandTest.email);
+  expect(test.reviews).toHaveLength(1);
+});
---
+++ api/src/tests/placeTests/get.place.test.ts
@@ -0,0 +1,50 @@
+const {
+  createPlace,
+  deletePlace,
+  addPlaceReview,
+  getPlaceByID,
+} = require("../../db/models/placeModel");
+const mongoose = require("mongoose");
+const connect = require("../../db/db");
+
+export const placeTest = {
+  capacity: "200",
+  name: "Local de prueba",
+  email: "soyunTest9@gmail.com",
+  password: "estaesunacontrasena",
+  hasSound: true,
+  city: "Bogota",
+  adress: "Avenida las americas",
+  description: "Esta es una descripción",
+  reviews: [],
+  dates: [],
+  availableDates: [],
+  pendingDates: [],
+  profilePicture: "not-working",
+};
+
+let place: any;
+beforeAll(async () => {
+  await connect();
+  place = await createPlace(placeTest);
+});
+
+afterAll(async () => {
+  await deletePlace(placeTest.email);
+  await mongoose.connection.close();
+});
+
+test("place should to be object", () => {
+  expect(typeof place).toBe("object");
+});
+test("place should to have property name", () => {
+  expect(place).toHaveProperty("name");
+});
+test("place should to be in db", async () => {
+  const placeById = await getPlaceByID(place._id);
+  expect(typeof placeById).toBe("object");
+});
+test("place should to be in db", async () => {
+  const placeById = await getPlaceByID(place._id);
+  expect(placeById).toHaveProperty("name");
+});
