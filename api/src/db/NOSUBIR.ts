b0c42949ee55a60ffc008d22107bfc3ab9e1f562
alann03 <alanhuismann03@gmail.com>
2022-08-19 11:47:17 -0300

    gitflow-feature-stash: CardDetail

--- api/src/db/NOSUBIR.ts
+++ api/src/db/NOSUBIR.ts
@@ -1,7 +1,2 @@
-<<<<<<< Updated upstream
-export const USERNAME = 'Admin-RockStartUser'
-export const PASSWORD = 'E8GQ0c2j0wKGyIEE'
-=======
 export const USERNAME = "alan03";
 export const PASSWORD = "332ByrxvdUSvLpMR";
->>>>>>> Stashed changes
