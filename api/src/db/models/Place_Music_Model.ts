f5dafa69c82f68a146149a374a70dafde353df17
Carlos Laprida <carloslapridat@gmail.com>
2022-08-25 15:49:34 -0300

    Ruta delete pending date terminada

--- api/src/db/models/Place_Music_Model.ts
+++ api/src/db/models/Place_Music_Model.ts
@@ -9,7 +9,11 @@ const placeSchemaModel = require("../schemas/placeSchema");
 const musicBand = model("musicband", musicBandSchema);
 const place = model("place", placeSchemaModel);
 
-export const addPendingDate = async (musicEmail: string, placeEmail: string, date: Date) => {
+export const addPendingDate = async (
+	musicEmail: string,
+	placeEmail: string,
+	date: Date
+) => {
 	try {
 		const currentMusicBand = await getMusicBand(musicEmail);
 		const currentPlace = await getPlace(placeEmail);
@@ -21,7 +25,7 @@ export const addPendingDate = async (musicEmail: string, placeEmail: string, dat
 						...currentPlace.pendingDates,
 						{ musicBand: currentMusicBand.name, date: date },
 					],
-				},
+				}
 			);
 			await musicBand.updateOne(
 				{ email: musicEmail },
@@ -30,7 +34,7 @@ export const addPendingDate = async (musicEmail: string, placeEmail: string, dat
 						...currentMusicBand.pendingDates,
 						{ place: currentPlace.name, date: date },
 					],
-				},
+				}
 			);
 			return { msg: "Se actualizó correctamente" };
 		} else {
@@ -40,6 +44,41 @@ export const addPendingDate = async (musicEmail: string, placeEmail: string, dat
 		return { error };
 	}
 };
+
+export const removePendingDate = async (
+	musicEmail: string,
+	placeEmail: string,
+	date: Date
+) => {
+	try {
+		const currentMusicBand = await getMusicBand(musicEmail);
+		const currentPlace = await getPlace(placeEmail);
+		if (currentMusicBand && currentPlace && date) {
+			await place.updateOne(
+				{ email: placeEmail },
+				{
+					pendingDates: [
+						currentPlace.pendingDates.filter((e: Date) => e !== date),
+					],
+				}
+			);
+			await musicBand.updateOne(
+				{ email: musicEmail },
+				{
+					pendingDates: [
+						currentMusicBand.pendingDates.filter((e: Date) => e !== date),
+					],
+				}
+			);
+			return { msg: "Se eliminó la fecha correctamente" };
+		} else {
+			return { error: "No se encontraron los usuarios" };
+		}
+	} catch (error: any) {
+		return { error };
+	}
+};
+
 /* 
 export const confirmedDate = async (musicEmail: string, placeEmail: string, date: Date) => {
 	try {
