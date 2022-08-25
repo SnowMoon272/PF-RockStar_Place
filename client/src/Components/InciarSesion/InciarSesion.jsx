662225532f287b6c8d255c7a8ef51646f54db840
Sebastian-pz <spezuluaga@gmail.com>
2022-08-25 15:56:12 -0500

    Logic for login and examples of how to render by user

--- client/src/Components/InciarSesion/InciarSesion.jsx
+++ client/src/Components/InciarSesion/InciarSesion.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import axios from "axios";
+import React, { useState } from "react";
 import styled from "styled-components";
 import NavBar from "../NavBar/NavBar";
 import Colors from "../../Utils/colors";
+import { isMusicband, isAdmin, isPlace } from "../../Utils/auth.controller";
 
 const RegisterStyleCont = styled.div`
   box-sizing: border-box;
@@ -27,11 +30,72 @@ const RegisterStyleContJr = styled.div`
   align-items: center;
 `;
 
+const BACK_URI = "http://localhost:3001";
+const user = () => {
+  if (isMusicband()) return <p>This is musicband component</p>;
+  if (isPlace()) return <p>This is place component</p>;
+  if (isAdmin()) return <p>This is admin component</p>;
+  return <p>Component by default</p>;
+};
+
 function InciarSesion() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    const response = await axios({
+      method: "POST",
+      data: {
+        email,
+        password,
+      },
+      withCredentials: true,
+      url: `${BACK_URI}/login`,
+    });
+    if (response) {
+      const { token } = response.data;
+      localStorage.setItem("user-token", token);
+      const header = new Headers();
+      header.append("authorization", token);
+      window.location.replace("http://localhost:3000/");
+    }
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user-token");
+  };
+
   return (
     <RegisterStyleCont>
       <NavBar LogIn FondoImg Home />
-      <RegisterStyleContJr>PROXIMAMENTE...</RegisterStyleContJr>
+      <RegisterStyleContJr>
+        <h3>Login</h3>
+        <label htmlFor="input-email">Email</label>
+        <input
+          name="input-email"
+          id="input_email"
+          type="text"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="input-password">Password</label>
+        <input
+          name="input-password"
+          id="input_password"
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" onClick={login}>
+          LOGIN
+        </button>
+        <button type="submit" onClick={logout}>
+          Logout
+        </button>
+        {
+          user()
+        }
+      </RegisterStyleContJr>
     </RegisterStyleCont>
   );
 }
