0a048434c8f6a043ef2019f7cff19f3b74edee4b
Matías Straface <matiasgstraface@gmail.com>
2022-08-27 15:35:47 -0300

    correccion componente pago

--- api/src/controllers/mercadoPago.controller.ts
+++ api/src/controllers/mercadoPago.controller.ts
@@ -1,13 +1,13 @@
 // import { any, any } from "express";
 
-const express = require('express');
+const express = require("express");
 
 export const f = {};
 
-const mercadopago = require('mercadopago');
+const mercadopago = require("mercadopago");
 
 mercadopago.configure({
-	access_token: 'TEST-2008030907073777-082315-afe1ae7f4b90cc0cb186397d52cd83f6-1184744317',
+	access_token: "TEST-2008030907073777-082315-afe1ae7f4b90cc0cb186397d52cd83f6-1184744317",
 });
 
 const checkOutController = async (req: any, res: any) => {
@@ -15,21 +15,20 @@ const checkOutController = async (req: any, res: any) => {
 		let preference = {
 			items: [
 				{
-					title: 'Suscripcion RockStar place',
+					title: "Suscripcion RockStar place",
 					quantity: 1,
 					unit_price: 500,
 				},
 			],
 			back_urls: {
-				failure: 'http://localhost:3000/suscripcionerror',
-				pending: 'http://localhost:3000/suscripcionerror',
-				success: 'http://localhost:3000/suscripcionsuccess',
+				failure: "http://localhost:3000/suscripcionerror",
+				pending: "http://localhost:3000/suscripcionerror",
+				success: "http://localhost:3000/suscripcionsuccess",
 			},
-			auto_return: 'approved',
+			auto_return: "approved",
 		};
 		mercadopago.preferences.create(preference).then(function (response: any) {
 			res.redirect(response.body.init_point);
-			console.log(response);
 		});
 	} catch (error) {
 		console.log(error);
