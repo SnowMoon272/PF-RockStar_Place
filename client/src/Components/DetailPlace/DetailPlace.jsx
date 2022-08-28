caf43a6aec3be2643c0c3ea0e0b329467121f5e5
Manuel Roberto Serrano Torres <CastielAltair0027@outlook.com>
2022-08-28 10:49:02 -0500

    Datos

--- client/src/Components/DetailPlace/DetailPlace.jsx
+++ client/src/Components/DetailPlace/DetailPlace.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import DetailPlaceBL from "./DetailPlaceBL";
 import DetailPlaceUNL from "./DetailPlaceNL";
+import DetailPlaceLL from "../PerfilesLogueados/PerfilLocal";
 import { isMusicband, isAdmin, isPlace } from "../../Utils/auth.controller";
 
 function DetailPlace() {
   if (isMusicband()) return <DetailPlaceBL />;
-  if (isPlace()) return <p>This is Place component</p>;
-  if (isAdmin()) return <p>This is admin component</p>;
+  if (isPlace()) return <DetailPlaceLL />;
+  if (isAdmin()) return <p>This is Place component</p>;
   return <DetailPlaceUNL />;
 }
 
