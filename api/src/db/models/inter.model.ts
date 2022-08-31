4bca5b7913ebc635fdbec14197e5dac62e18da3c
Sebastian-pz <spezuluaga@gmail.com>
2022-08-31 16:28:13 -0500

    no merge

--- api/src/db/models/inter.model.ts
+++ api/src/db/models/inter.model.ts
@@ -32,11 +32,40 @@ export const sendNotification = async (
  *	@param {string} email recibe por parametro el email de la cuenta que será modificada
  *	@return {object} Retorna la respuesta del update
  */
-export const deleteAllNotifications = async (email: string, modelo: any) => {
+export const deleteAllNotifications = async (modelo: any, email: string) => {
 	try {
 		const res = modelo.update({ email }, { notifications: [] });
 		return res;
 	} catch (error) {
 		return { error: `Error deleting, (${error})` };
+	}
+};
+
+/**
+ *	getNotifications es la función encargada traer todas las notificaciones de un usuario
+ *
+ *	@param {Model} modelo recibe por parametro el modelo de mongoose que será utilizada
+ *	@param {string} email recibe por parametro el email de la cuenta a la que se quieren acceder a sus notificaciones
+ *	@return {object} Retorna un arreglo con las notificaiones del usuario
+ */
+export const getNotifications = async (modelo: any, email: string) => {
+	try {
+		const notifications = await modelo.findOne({ email }, { notifications: 1 });
+		return notifications;
+	} catch (error) {
+		return { error: `Error in getter: (${error})` };
+	}
+};
+
+//Testing
+export const markAsRead = async (model: any, email: string, id: string) => {
+	try {
+		const mark = await model.update(
+			{ "notifications._id": id },
+			{ "notifications.isNew": false }
+		);
+    return mark;
+	} catch (error) {
+		return { error: `Error in mark as read: (${error})` };
 	}
 };
