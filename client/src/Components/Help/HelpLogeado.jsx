c8664da19e6f4591826e9e7d82db3f9b168739ea
alann03 <alanhuismann03@gmail.com>
2022-09-01 01:19:59 -0300

    Se arregló el paginado de UNL y se importaron useStates faltantes

--- client/src/Components/Help/HelpLogeado.jsx
+++ client/src/Components/Help/HelpLogeado.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Colors from "../../Utils/colors";
 import Logo from "../../Assets/img/LogoCircular.png";
--- client/src/Components/Help/HelpNoLogeado.jsx
+++ client/src/Components/Help/HelpNoLogeado.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Colors from "../../Utils/colors";
 import Logo from "../../Assets/img/LogoCircular.png";
