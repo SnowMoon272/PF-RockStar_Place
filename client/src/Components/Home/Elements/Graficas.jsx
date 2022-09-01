a402a877df7704c4711d5fa9b1f230475e893bb9
Matías Straface <matiasgstraface@gmail.com>
2022-09-01 16:41:43 -0300

    instalación libreria y graficas para ADMIN

--- client/src/Components/Home/Elements/Graficas.jsx
+++ client/src/Components/Home/Elements/Graficas.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+import GraficaFechas from "./GraficaFechas";
+import GraficaRegistrados from "./GraficaRegistrados";
 
 function Graficas() {
-  return <h1>Graficas</h1>;
+  const GraficasStyleCont = styled.div`
+    height: 75%;
+    width: 80%;
+    display: flex;
+    background-color: #ffffffae;
+    align-items: center;
+    margin-left: 10%;
+    margin-top: 7%;
+    justify-content: space-around;
+  `;
+
+  return (
+    <GraficasStyleCont>
+      <GraficaFechas />
+      <GraficaRegistrados />
+    </GraficasStyleCont>
+  );
 }
 
 export default Graficas;
