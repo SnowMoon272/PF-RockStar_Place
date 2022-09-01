d5cb8f835ecede891ab811cd027ec3fd03e10249
Matías Straface <matiasgstraface@gmail.com>
2022-09-01 01:25:54 -0300

    el misterio de los useStates perdidos

--- client/src/Components/Suscribete/SuscripcionError.jsx
+++ client/src/Components/Suscribete/SuscripcionError.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import BotonSuscribete from "./BotonSuscripcion";
 import NavBar from "../NavBar/NavBar";
