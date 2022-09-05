dd0d9fe3f2dcef353a51f268f0e743e3b370a5ed
Matías Straface <matiasgstraface@gmail.com>
2022-09-04 22:56:47 -0300

    template cambio clave

--- api/src/controllers/nodeMailer.controller.ts
+++ api/src/controllers/nodeMailer.controller.ts
@@ -10,6 +10,7 @@ const {
 	bannedTemplate,
 	cancelMusicMatchTemplate,
 	cancelPlaceMatchTemplate,
+	updatePasswordTemplate,
 } = require("../emailTemplates");
 
 export const f = {};
@@ -18,19 +19,14 @@ const SERVICE = "gmail";
 const AUTH = {
 	TYPE: "OAuth2",
 	USER: "rockstarplaceok@gmail.com",
-	CLIENT_ID:
-		"193709480522-3cfatqmpnvh1p4ut1pnmhi34d1jbunmq.apps.googleusercontent.com",
+	CLIENT_ID: "193709480522-3cfatqmpnvh1p4ut1pnmhi34d1jbunmq.apps.googleusercontent.com",
 	CLIENT_SECRET: "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",
 	REDIRECT_URI: "https://developers.google.com/oauthplayground",
 	REFRESH_TOKEN:
 		"1//04Xez4hBWpof2CgYIARAAGAQSNwF-L9IrKowoW8CKG3yWaxZD1TLuTpJAW3r0DPqWrlTtHhFD61BxYfDFq6W4gcLtyVG2XY6Q2OQ",
 };
 
-const oAuth2Client = new google.auth.OAuth2(
-	AUTH.CLIENT_ID,
-	AUTH.CLIENT_SECRET,
-	AUTH.REDIRECT_URI
-);
+const oAuth2Client = new google.auth.OAuth2(AUTH.CLIENT_ID, AUTH.CLIENT_SECRET, AUTH.REDIRECT_URI);
 
 oAuth2Client.setCredentials({ refresh_token: AUTH.REFRESH_TOKEN });
 
@@ -73,8 +69,7 @@ const cancelMatchController = async (req: any, res: any) => {
 				};
 
 				let result =
-					(await transporter.sendMail(placeMail)) &&
-					(await transporter.sendMail(musicMail));
+					(await transporter.sendMail(placeMail)) && (await transporter.sendMail(musicMail));
 				return result;
 			} catch (error) {
 				console.log(error);
@@ -129,8 +124,7 @@ const matchMailController = async (req: any, res: any) => {
 				};
 
 				let result =
-					(await transporter.sendMail(placeMail)) &&
-					(await transporter.sendMail(musicMail));
+					(await transporter.sendMail(placeMail)) && (await transporter.sendMail(musicMail));
 				return result;
 			} catch (error) {
 				console.log(error);
@@ -230,9 +224,49 @@ const registerMailController = async (req: any, res: any) => {
 	}
 };
 
+const updatePasswordMailController = async (req: any, res: any) => {
+	const { email } = req.params;
+	if (email) {
+		async function sendMail() {
+			try {
+				let accessToken = await oAuth2Client.getAccessToken();
+				let transporter = nodeMailer.createTransport({
+					service: SERVICE,
+					tls: { rejectUnauthorized: false },
+					auth: {
+						type: AUTH.TYPE,
+						user: AUTH.USER,
+						clientId: AUTH.CLIENT_ID,
+						clientSecret: AUTH.CLIENT_SECRET,
+						refreshToken: AUTH.REFRESH_TOKEN,
+						accessToken: accessToken,
+					},
+				});
+				let mailOptions = {
+					from: `RockStar Place <${AUTH.USER}>`,
+					to: email,
+					subject: "Cambio de contraseña",
+					html: updatePasswordTemplate(email),
+				};
+
+				let result = await transporter.sendMail(mailOptions);
+				return result;
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		sendMail()
+			.then((result) => res.status(200).send("enviado"))
+			.catch((error) => console.log(error.message));
+	} else {
+		return res.status(404).send("No se pudo enviar el email");
+	}
+};
+
 module.exports = {
 	bannedEmailController,
 	registerMailController,
 	matchMailController,
 	cancelMatchController,
+	updatePasswordMailController,
 };
