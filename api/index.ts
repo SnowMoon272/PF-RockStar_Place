3f675f3ab48bb0660a854c6fb80fac0b008571d4
Sebastian Pérez <87543572+Sebastian-pz@users.noreply.github.com>
2022-09-06 14:36:09 -0500

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -26,7 +26,7 @@ server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(morgan("dev"));
 server.get("/", (req: any, res: any) => {
-	res.setHeader("Access-Control-Allow-Origin", process.env.FRONT_VERCEL || "http://localhost:3000");
+	res.setHeader("Access-Control-Allow-Origin", "https://pf-rock-star-place.vercel.app");
 	res.setHeader("Access-Control-Allow-Credentials", "true");
 	res.setHeader("Access-Control-Max-Age", "1800");
 	res.setHeader("Access-Control-Allow-Headers", "content-type");
